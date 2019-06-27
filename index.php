64ff90073727352a6daef920bfdcbb98d3d204c1
Alex Teterin <at28813905@gmail.com>
2019-06-27 21:03:55 +0300

    проверка переменной text

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
-<?php 
-
-include('vendor/autoload.php'); //Подключаем библиотеку
-use Telegram\Bot\Api; 
-
-$telegram = new Api('832044822:AAEb48OoiZoxf4YTrS3T3-Z1GWcugj_VMcE'); //Устанавливаем токен, полученный у BotFather
-$result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
-echo $result;    
-$text = $result["message"]["text"]; //Текст сообщения
-$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
-
-if($name) {
-	$reply = "Добро пожаловать в бота!";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+<?php
+	include('vendor/autoload.php'); //Подключаем библиотеку
+    use Telegram\Bot\Api;
+    $telegram = new Api('885752742:AAF63rND57OidzVAJ3ReDp7qGkX7oVaunBY'); //Устанавливаем токен, полученный у BotFather
+    $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    $text = $result["message"]["text"]; //Текст сообщения
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["Сократить ссылку"],["Расшифровать ссылку"]]; //Клавиатура
+    if($text)
+    {
+        if ($text == "/start")
+        {
+            if (strlen($name) != 0)
+            {
+                $reply = "Добро пожаловать, ".$name."!";
+            }
+            else
+            {
+                $reply = "Добро пожаловать, Незнакомец";
+            }
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }
 }
