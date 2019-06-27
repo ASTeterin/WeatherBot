20e720992f2f0a1aee959b207bcef41f496ef126
Alex Teterin <at28813905@gmail.com>
2019-06-28 00:27:47 +0300

    test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     use Telegram\Bot\Api;
     $telegram = new Api('832044822:AAEb48OoiZoxf4YTrS3T3-Z1GWcugj_VMcE'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    var_dump($result);
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
