89dabfc4ff43fe7c5978bffc627835b14aa07355
Alex Teterin <at28813905@gmail.com>
2019-06-27 23:49:19 +0300

    1

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
     if($text)
     {
+	 if ($text == '/start') {
+            if (strlen($name) == 0) {
+                $reply = 'Добро пожаловать, Незнакомец!';
+            }
+            else {
+                $reply = 'Добро пожаловать, '.$name.'!';
+            }
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
        
 }else{
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
