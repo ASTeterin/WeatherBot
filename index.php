4f3e21fd2cdf22ce532deb6dc1deade2d3fcc35d
Alex Teterin <at28813905@gmail.com>
2019-06-28 19:47:07 +0300

    .

--- index.php
+++ index.php
@@ -7,7 +7,19 @@
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-
+    
+    function getDataFromApi($url)
+    {
+        $ch = curl_init();
+        // установка URL и других необходимых параметров
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        // загрузка страницы и выдача её браузеру
+        $data = curl_exec($ch);
+        // завершение сеанса и освобождение ресурсов
+        curl_close($ch);
+        return $data;
+    }
 
     $keyboard = [["/sayhello"],["/start"]]; 
     if($text){
@@ -24,7 +36,10 @@
             }
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }else{
-            $reply = "неизвестная комманда " .$text ;
+            //$reply = "неизвестная комманда " .$text ;
+            $url = "http://api.apixu.com/v1/forecast.json?key=a063d1eac8054ab392f195555192506&q=%D0%92%D0%BE%D0%BB%D0%B6%D1%81%D0%BA&days=1&lang=ru";
+//
+            $reply = getDataFromApi($url);
 	    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }
     }
