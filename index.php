24602ce3a1dbf90dd96ef4984a990add5476e274
Alex Teterin <at28813905@gmail.com>
2019-06-28 13:56:40 +0300

    1

--- index.php
+++ index.php
@@ -4,25 +4,15 @@
     $telegram = new Api('832044822:AAEb48OoiZoxf4YTrS3T3-Z1GWcugj_VMcE'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
     $text = $result["message"]["text"]; //Текст сообщения
-$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-$keyboard = [["/sayhello"],["/help"]]; //Клавиатура
-if($text){
-    if ($text == "/start") {
-        $reply = "Добро пожаловать в бота!";
-        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-    }elseif ($text == "/help") {
-        $reply = "Информация с помощью.";
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    }elseif ($text == "/sayhello") {
-        $reply = "Привет, " . $name;
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    }else{
-        $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
-    }
-}else{
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
-}
+    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    $keyboard = [["/sayhello"],["/help"]]; //Клавиатура
+    
+    if($text){
+		if ($text == "/sayhello") {
+			$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $name ]);
+		}
+	}else{
+		$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+	}
 ?>
