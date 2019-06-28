309d5a5d527b98ca186d6e3eb92451e9f94b7e89
Alex Teterin <at28813905@gmail.com>
2019-06-28 14:35:10 +0300

    test

--- index.php
+++ index.php
@@ -9,11 +9,23 @@
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
 
 
+    $keyboard = [["/sayhello"],["/help"]]; //??????????
     if($text){
-        if ($text == "/sayhello") {
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $name ]);
+        if ($text == "/start") {
+            
+            $reply = "Hello!";
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }elseif ($text == "/help") {
+           
+            $reply = "Info.";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }elseif ($text == "/sayhello") {
+  
+            $reply = "Hello, " . $name;
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }else{
+            $reply = "?? ??????? \"<b>".$text."</b>\" ?????? ?? ???????.";
         }
-    }else{
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
 ?>
