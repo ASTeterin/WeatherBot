000d28536d1ba9bd9c7f12878a66c2375588b427
Alex Teterin <at28813905@gmail.com>
2019-06-28 10:02:50 +0300

    test

--- index.php
+++ index.php
@@ -3,17 +3,16 @@
     use Telegram\Bot\Api;
     $telegram = new Api('832044822:AAEb48OoiZoxf4YTrS3T3-Z1GWcugj_VMcE'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
-    //print_r($result);
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-    $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+    //$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
     if($text == "/sayhello") {
         $response = "Привет";
-        if ($name == "Саша") {
+        /*if ($name == "Саша") {
             $response = $response . $name;
-        }
+        }*/
 	 
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $response ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $result["message"] ]);
        
 }else{
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
