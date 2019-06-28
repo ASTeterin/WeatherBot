5e216589a02a0174236e644a39c3d8529e1e53fe
Alex Teterin <at28813905@gmail.com>
2019-06-28 14:42:29 +0300

    добавлена проверка пустого имени

--- index.php
+++ index.php
@@ -16,13 +16,13 @@
             $reply = "Hello!";
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-        }elseif ($text == "/help") {
-           
-            $reply = "Info.";
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }elseif ($text == "/sayhello") {
-  
-            $reply = "Hello, " . $name;
+            $reply = "Hello, ";
+            if (empty($name)) {
+                $reply .= "noname";
+            } else {
+                $reply .= $name;
+            }
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }else{
             $reply = "?? ??????? \"<b>".$text."</b>\" ?????? ?? ???????.";
