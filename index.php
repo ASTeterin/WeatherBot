66308495b7b2c5d978ce6f07487226209a79446b
Alex Teterin <at28813905@gmail.com>
2019-06-30 01:30:42 +0300

    test

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             //$reply = "неизвестная комманда " .$text ;
             $url = "http://api.apixu.com/v1/forecast.json?key=a063d1eac8054ab392f195555192506&q=" . $text . "&days=1&lang=ru";
 //
-            $reply = getDataFromApi($url);
+            $str = getDataFromApi($url);
 	$forecast = explode("\"date\":\"", $str);
 //print_r($forecast);
 
