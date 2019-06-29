93f5fc91c13fd2004b0aafc013400731f321ffd8
Alex Teterin <at28813905@gmail.com>
2019-06-30 01:55:30 +0300

    1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     
 
     include('vendor/autoload.php'); //Подключаем библиотеку
-    require_once("inc\common.inc.php");
+    require_once('inc/common.inc.php');
     use Telegram\Bot\Api;
     $telegram = new Api('832044822:AAEb48OoiZoxf4YTrS3T3-Z1GWcugj_VMcE'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
