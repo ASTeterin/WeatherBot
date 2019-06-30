1a7f1b89d3514ce2c961d9439f12a6d320a194d5
Alex Teterin <at28813905@gmail.com>
2019-06-30 23:40:54 +0300

    1

--- index.php
+++ index.php
@@ -18,9 +18,9 @@
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
         }elseif ($text == "/help") {
-            $reply = "Бот позволяет посмотреть прогноз погоды в любых населенных пунктах.<br>" .
+            $reply = "Бот позволяет посмотреть прогноз погоды в любых населенных пунктах.<br>"; /*.
                 "Список комманд:" . "<br><населенный пункт> <количество дней> - предоставляет прогноз погоды в <населенный пункт> на <количество дней>. " .
-                "Если количество дней не указано - будет выдан прогноз на текущий день";  
+                "Если количество дней не указано - будет выдан прогноз на текущий день";  */
             $telegram->sendMessage([ 'chat_id' => $chat_id,  'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
         }elseif ($text == "/sayhello") {
             $reply = "Здравствуйте, ";
