8baf70be60ae87d64c1c8ab3d3dd3a4dda06f372
Alex Teterin <at28813905@gmail.com>
2019-07-01 18:50:39 +0300

    showForecast

--- inc/bot.inc.php
+++ inc/bot.inc.php
@@ -29,29 +29,38 @@ function helpBot($telegram, $chat_id)
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
 }
 
+function showForecast($telegram, $chat_id, $text)
+{
+    list($city, $days) = explode(" ", removeExtraSymbols($text, " ")) ;
+    $url = "http://api.apixu.com/v1/forecast.json?key=a063d1eac8054ab392f195555192506&q=" . urlencode($city) . "&days=" . $days . "&lang=ru";
+    $str = getDataFromApi($url);
+    if (!strpos($str, "error"))
+    {
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $city ]);
+        $forecast = explode("\"date\":\"", $str);
+
+        $weather = parseForecast($forecast);
+        for ($i = 1; $i < count($weather); $i++) {
+            //$reaply = ""; 
+            $reply = $weather[$i]['date'] . " " . $weather[$i]['rain'] . ". Минимальная температура " . $weather[$i]['min_temp'] . ", максимальная температура " . $weather[$i]['max_temp'];
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }
+    }else{
+        $reply = "Населенный пункт " . '<b>' . $city . '</b>' . " не найден";
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+    }  
+}
+
 function botWorking($telegram, $chat_id, $text, $name, $keyboard)
 {
     if($text){
         if ($text == "/start") {
             startBot($telegram, $chat_id, $keyboard, $name);
-            /*$reply = "Погода в городах мира!";
-            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);*/
         }elseif ($text == "/help") {
-            /*$reply = "Бот позволяет посмотреть прогноз погоды в любых населенных пунктах. "
-                    . "Для вывода информации введите название населенного пункта и количество дней, на которые необходим прогноз " ;
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);*/
             helpBot($telegram, $chat_id);
-        }elseif ($text == "/sayhello") {
-            $reply = "Здравствуйте, ";
-            if (empty($name)) {
-                $reply .= "незнакомец";
-            } else {
-                $reply .= $name;
-            }
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }else{
-            list($city, $days) = explode(" ", removeExtraSymbols($text, " ")) ;
+            showForecast($telegram, $chat_id, $text);
+            /*list($city, $days) = explode(" ", removeExtraSymbols($text, " ")) ;
             $url = "http://api.apixu.com/v1/forecast.json?key=a063d1eac8054ab392f195555192506&q=" . urlencode($city) . "&days=" . $days . "&lang=ru";
             $str = getDataFromApi($url);
             if (!strpos($str, "error"))
@@ -68,7 +77,7 @@ function botWorking($telegram, $chat_id, $text, $name, $keyboard)
             }else{
                 $reply = "Населенный пункт " . '<b>' . $city . '</b>' . " не найден";
                 $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-            }
+            }*/
         }
     }    
 }
