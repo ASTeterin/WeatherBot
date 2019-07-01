6d552aca720499b18ef024a3cafd16bd98f08a54
Alex Teterin <at28813905@gmail.com>
2019-07-02 00:08:30 +0300

    remove comment

--- inc/api.inc.php
+++ inc/api.inc.php
@@ -3,14 +3,13 @@
     function getDataFromApi($url): ?string
     {
         $ch = curl_init();
-        // установка URL и других необходимых параметров
+        
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        // загрузка страницы и выдача её браузеру
         $data = curl_exec($ch);
-        // завершение сеанса и освобождение ресурсов
+
         curl_close($ch);
         return $data;
     }
