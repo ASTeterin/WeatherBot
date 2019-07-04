ff9875ec0b14d2731242505f6fb76f5aa17f95c2
Alex Teterin <at28813905@gmail.com>
2019-07-04 13:34:24 +0300

    rollback

--- inc/bot.inc.php
+++ inc/bot.inc.php
@@ -58,8 +58,8 @@ function showForecast($telegram, $chat_id, $text, &$keyboard)
         foreach ($weather['forecast'] as $dailyForecast) {
             $reply = $dailyForecast['date'] . ": " . $dailyForecast['condition'] . ". \nМинимальная температура " . $dailyForecast['min_temp'] . "\nМаксимальная температура " . $dailyForecast['max_temp'];
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply]);
-            $url = "https://" . $dailyForecast['condition']['icon'];
-            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+            /*$url = "https://" . $dailyForecast['condition']['icon'];
+            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);*/
         }
     }else{
         $reply = "Населенный пункт " . '<b>' . $city . '</b>' . " не найден";
