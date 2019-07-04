220d3a34b00999aa01f7509cb810c0af0d070196
Alex Teterin <at28813905@gmail.com>
2019-07-04 09:05:24 +0300

    fix error showForecast

--- inc/bot.inc.php
+++ inc/bot.inc.php
@@ -50,7 +50,7 @@ function showForecast($telegram, $chat_id, $text, &$keyboard)
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $city ]);
         //$forecast = explode("\"date\":\"", $response);
          
-        $weather = parseForecast($forecast);
+        $weather = parseForecast($response);
         for ($i = 1; $i < count($weather); $i++) {
             $reply = $weather[$forecat]['date'] ;//. " " . $weather[$i][''] . ". \nМинимальная температура " . $weather[$i]['min_temp'] . "\nМаксимальная температура " . $weather[$i]['max_temp'];
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
